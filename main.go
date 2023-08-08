1e97aca8d795a9ac3aa838e16acfedfaec538a47
Aidyn Torgayev <a.torgayev@arbuz.kz>
2023-08-08 18:52:49 +0600

    download from multiple urls

--- main.go
+++ main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"chunker"
 	"downloader"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path"
 	"sync"
 )
 
 var (
-	fileURLFlag          = flag.String("url", "https://filesamples.com/samples/video/mp4/sample_1280x720_surfing_with_audio.mp4", "URL of the file to download")
 	chunkSizeFlag        = flag.Int("chunkSize", 1024*1024*10, "Size of each chunk in bytes")
 	numWorkersFlag       = flag.Int("numWorkers", 5, "Number of workers to use")
 	chunkPrefixFlag      = flag.String("chunkPrefix", "chunk-", "Prefix of the chunk files")
 	forceAlternativeFlag = flag.Bool("forceAlternative", false, "Force the alternative downloader to be used")
+	urlsFileFlag         = flag.String("urlsFile", "urls.txt", "File containing the urls to download")
 )
 
 func main() {
 	flag.Parse()
 
-	fileURL := *fileURLFlag
 	chunkSize := *chunkSizeFlag
 	numWorkers := *numWorkersFlag
 	chunkPrefix := *chunkPrefixFlag
 	forceAlternative := *forceAlternativeFlag
-	fileName := path.Base(fileURL)
+	urlsFile := *urlsFileFlag
+
+	urls, err := readURLs(urlsFile)
+	if err != nil {
+		log.Fatalf("Error reading urls file: %v\n", err)
+	}
+
 	c := chunker.NewChunker(chunkPrefix, chunkSize)
 	wg := &sync.WaitGroup{}
 
-	if err := download(fileURL, chunkSize, numWorkers, c, forceAlternative, wg); err != nil {
+	fileName, err := download(urls, chunkSize, numWorkers, c, forceAlternative, wg)
+	if err != nil {
 		log.Fatalf("Error downloading file: %v\n", err)
 	}
 
@@ -44,19 +52,22 @@ func main() {
 	}
 }
 
-func download(fileURL string, chunkSize int, numWorkers int, c chunker.Chunker, forceAlternative bool, wg *sync.WaitGroup) error {
-	url := fileURL
+func download(urls []string, chunkSize int, numWorkers int, c chunker.Chunker, forceAlternative bool, wg *sync.WaitGroup) (string, error) {
+	url := urls[0]
+	fileName := path.Base(url)
 
 	log.Printf("Downloading file from: %s\n", url)
 
-	resp, err := http.Get(url)
+	resp, err := http.Head(url)
 	if err != nil {
-		return fmt.Errorf("error downloading file: %v", err)
+		return "", fmt.Errorf("error downloading file: %v", err)
 	}
 	defer resp.Body.Close()
 
 	supportsRange := resp.Header.Get("Accept-Ranges") == "bytes"
 	fileSize := resp.ContentLength
+
+	downloader := newDownloader(chunkSize, numWorkers, fileSize, supportsRange, c, forceAlternative, wg)
 
 	// Generate chunk tasks
 	wg.Add(1)
@@ -67,9 +78,15 @@ func download(fileURL string, chunkSize int, numWorkers int, c chunker.Chunker, 
 		close(c.GetChunkChannel())
 	}(c, wg, fileSize)
 
-	downloader := newDownloader(chunkSize, numWorkers, resp.ContentLength, supportsRange, c, forceAlternative, wg)
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			downloader.Download(url)
+		}(url)
+	}
 
-	return downloader.Download(url)
+	return fileName, nil
 }
 
 func newDownloader(chunkSize int, numWorkers int, fileSize int64, supportsRange bool, chunker chunker.Chunker, forceAlternative bool, wg *sync.WaitGroup) downloader.Downloader {
@@ -93,4 +110,35 @@ func newDownloader(chunkSize int, numWorkers int, fileSize int64, supportsRange 
 	}
 
 	return d
+}
+
+func readURLs(urlsFile string) ([]string, error) {
+	urls := []string{}
+
+	file, err := os.Open(urlsFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		url := scanner.Text()
+
+		if url == "" {
+			continue
+		}
+
+		urls = append(urls, url)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	if len(urls) == 0 {
+		return nil, fmt.Errorf("no urls found in file")
+	}
+
+	return urls, nil
 }
